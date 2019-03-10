d660fb1e9475866a306d0e160097083aeaa91f52
Victor Alexandre <48135635+alxandrvictor@users.noreply.github.com>
2019-03-10 20:17:11 -0300

    Update exercicio1.php

--- exercicio1.php
+++ exercicio1.php
@@ -1,1 +1,11 @@
+<?php
 
+	print"Conversão de medidas";
+	print"\n\n";
+
+	$metros = (int)fgets(STDIN);
+
+	$conversaomm = $metros / 1000;
+	print"\n\n";
+
+	print"Você terá $conversaomm milímetros";
