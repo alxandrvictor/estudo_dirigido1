f9f4f127cfac86079ffc6feff4b75cbfd7ac0e9b
Victor Alexandre <48135635+alxandrvictor@users.noreply.github.com>
2019-03-10 20:36:19 -0300

    Create exercicio2.php

---
+++ exercicio2.php
@@ -0,0 +1,20 @@
+	print"Quantidade de dias: ";
+	$dias = (int)fgets(STDIN);          //Insira os dias
+	print"\n\n";
+
+	print"Quantidade de horas: ";
+	$horas = (int)fgets(STDIN);         //Insira as horas
+	print"\n\n";
+
+	print"Quantidade de minutos: ";
+	$minutos = (int)fgets(STDIN);       //Insira os minutos
+	print"\n\n";
+
+	print"Quantidade de segundos: ";
+	$segundos = (int)fgets(STDIN);     // Insira os segundos
+	print"\n\n";
+
+	$totalseconds = ((($dias/24/60/60)+$horas/60/60)+$minutos/60)+$segundos;
+
+	print"$dias dias, $horas horas, $minutos minutos, segundos, são $totalseconds segundos";
+	print"\n\n"; 
